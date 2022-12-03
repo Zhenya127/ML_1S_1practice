3bb04e962da8b1d0fea776ff1b3a42b9ed79f714
Semen Bakulin <percalee@gmail.com>
2022-12-03 03:03:25 -0800

    Description method

--- text_generator.py
+++ text_generator.py
@@ -13,6 +13,20 @@ generator = pipeline('text-generation', model='gpt2')
 def root():
     return {"message": "API application for text generator"}
 
+@app.get("/description/")
+def description():
+    message = {
+        'header': 'This project is built around gpt2 model',
+        'body': [
+            {'type': 'text_field', 'text': 'gpt2 is text generator model'},
+            {'type': 'text_field', 'text': 'This model was loaded from Hugging face hub'},
+            {'type': 'text_field', 'text': 'To test model use method `predict`'},
+            {'type': 'text_field', 'text': '`predict` requires body with single keyword(=text)'},
+            {'type': 'text_field', 'text': '`predict` returns generated text by gpt2 model'}
+        ]
+    }
+    return message
+
 @app.post("/predict/")
 def predict(item: Item):
     return generator(item.text, max_length=40, num_return_sequences=3)
