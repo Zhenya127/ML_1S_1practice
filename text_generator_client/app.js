27b921e383e7ee01e60ee10ad0796a1908abe2ee
Danil Makushev <reurairin@gmail.com>
2023-04-05 19:47:42 +0400

    add dockerfile

--- text_generator_client/app.js
+++ text_generator_client/app.js
@@ -1,4 +1,4 @@
-const url = 'http://51.250.110.185:8000/predict';
+const url = 'http://127.0.0.1:8000/predict';
 
 const input = document.getElementById('gen__input');
 const result = document.getElementById('gen__result');
